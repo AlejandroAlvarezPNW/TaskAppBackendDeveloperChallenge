c163570bf6e711d86610a4b83028e80c0cd3ecfd
Alejandro Alvarez <94766480+AlejandroAlvarezPNW@users.noreply.github.com>
2025-04-15 14:27:28 -0500

    More env variables changes

--- express-server/client/src/api.js
+++ express-server/client/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_NODE_ENV === 'localhost' ? 'http://localhost:3000/api': 'https://taskappdeployable.herokuapp.com/api';
+const url = process.env.REACT_APP_NODE_ENV === 'Production' ? 'https://taskappdeployable.herokuapp.com/api' : 'http://localhost:3000/api';
+console.log("url:", url);
 console.log("REACT_APP_NODE_ENV:Frontend:", process.env.REACT_APP_NODE_ENV);
 const api = axios.create({
     baseURL: url,
