d2c17f29f0678c7669c0bf7673c5ff31792df782
Alejandro Alvarez <94766480+AlejandroAlvarezPNW@users.noreply.github.com>
2025-04-15 16:28:47 -0500

    fixed url for requests

--- express-server/client/src/api.js
+++ express-server/client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url = process.env.REACT_APP_NODE_ENV === 'Production' ? 'https://taskappdeployable.herokuapp.com/api' : 'http://localhost:3000/api';
-//const url = 'https://taskappdeployable.herokuapp.com/api'; //This is needed during build time for the front-end
+const url = process.env.REACT_APP_NODE_ENV === 'Production' ? 'https://taskappdeployable-2815f043dfd0.herokuapp.com/api' : 'http://localhost:3000/api';
+//const url = 'https://taskappdeployable-2815f043dfd0.herokuapp.com/api'; //This is needed during build time for the front-end
 console.log("url:", url);
 console.log("REACT_APP_NODE_ENV:Frontend:", process.env.REACT_APP_NODE_ENV);
 const api = axios.create({
