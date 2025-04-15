55d411400851bdd0b48f17a6f9953bb8893cabde
Alejandro Alvarez <94766480+AlejandroAlvarezPNW@users.noreply.github.com>
2025-04-15 00:39:05 -0500

    Modified Env Variables

--- express-server/client/src/api.js
+++ express-server/client/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.NODE_ENV === 'Production' ? 'https://taskappdeployable.herokuapp.com/api' : 'http://localhost:3000/api';
+const url = process.env.REACT_APP_NODE_ENV === 'localhost' ? 'http://localhost:3000/api': 'https://taskappdeployable.herokuapp.com/api';
 const api = axios.create({
     baseURL: url,
     headers: { "Content-Type": "application/json" },
