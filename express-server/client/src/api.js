6c8f4a2632f19e9ae41300eedfae8fc10d2d1b2d
Alejandro Alvarez <94766480+AlejandroAlvarezPNW@users.noreply.github.com>
2025-04-15 00:54:45 -0500

    Modified For Debugging

--- express-server/client/src/api.js
+++ express-server/client/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const url = process.env.REACT_APP_NODE_ENV === 'localhost' ? 'http://localhost:3000/api': 'https://taskappdeployable.herokuapp.com/api';
+console.log("REACT_APP_NODE_ENV:Frontend:", process.env.REACT_APP_NODE_ENV);
 const api = axios.create({
     baseURL: url,
     headers: { "Content-Type": "application/json" },
